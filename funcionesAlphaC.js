8e8e3da7c40f382b1840f4342b4e060abb511e6b
FedericoCaceres101 <fedecaceres_8@hotmail.com>
2020-06-11 20:11:32 -0300

    Se agregaron las funciones para dibujar las orbitas y las estrellas

--- funcionesAlphaC.js
+++ funcionesAlphaC.js
@@ -7,17 +7,7 @@
 
 var planetas = [
     {
-        nombre:"Aplha Centauri A",
-        x:400,
-        y:245,
-        orbita: 45,             // Radio de la orbita
-        tamanio: 6,             // Radio del planeta
-        color: "#faf3ff",
-        mostrar: true,
-        velocidad: 0.0172,
-        pos_rad:23
-    },{
-        nombre:"Proxima Centauri",
+        nombre:"Star planet",
         x:362,
         y: 268,
         orbita: 80,
@@ -27,10 +17,10 @@ var planetas = [
         velocidad: 0.0107,
         pos_rad:0
     },{
-        nombre:"Star planet",
+        nombre:"Alpha Centauri B",
         x:389,
         y: 105,
-        orbita: 93,
+        orbita: 150,
         tamanio: 12,
         color: "#3cff8e",
         mostrar: true,
@@ -39,7 +29,7 @@ var planetas = [
     }
 ];
 
-function dibujar_planetas() {
+function dibujarEstrellas() {
     var canvas = document.getElementById('Canvas');
     var ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -53,5 +43,50 @@ function dibujar_planetas() {
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
+    for(var i of planetas){
+
+        ctx.beginPath();
+        ctx.moveTo(cenX+i.orbita,200);
+        ctx.arc(cenX, cenY, i.orbita, 0, 2 * Math.PI, false);
+        ctx.strokeStyle = "white";
+        ctx.stroke();
+        ctx.closePath();
+
+        if(i.mostrar) {
+            //planetas
+            ctx.beginPath();
+            ctx.fillStyle = i.color;
+            ctx.moveTo(i.x, i.y);
+            ctx.arc(i.x, i.y, i.tamanio, 0, 2 * Math.PI, false);
+            ctx.stroke();
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+var interval;
+
+var a=0;
+
+function moverplanetas() {
+    clearInterval(interval);
+    var x = document.forms["formulario"]["fechayear"].value/2000;
+
+
+    for(var i of planetas){
+        i.pos_rad += x;
+        i.x= 400 + i.orbita* Math.cos(i.pos_rad);
+        i.y= 200 + i.orbita* Math.sin(i.pos_rad);
+    }
+
+    interval=setInterval(animacion, 10);
 }
 
+function animacion(){
+    for(var i of planetas){
+        i.pos_rad += i.velocidad;
+        i.x= 400 + i.orbita* Math.cos(i.pos_rad);
+        i.y= 200 + i.orbita* Math.sin(i.pos_rad);
+    }
+    dibujarEstrellas();
+}
