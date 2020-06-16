f4bf25cb39ac2cf3a7e1872ab73ffa1ed379f561
FKaranicolas <facukaranicolas@gmail.com>
2020-06-16 20:25:14 -0300

    SE TERMINO!!!!

--- funcionesAlphaC.js
+++ funcionesAlphaC.js
@@ -1,11 +1,11 @@
 /**
  * Dibuja los planetas
- * @method dibujar_planetas
+ * @method dibujar_estrellas
  * @param Parámetro A
  * @return Valor que retorna
  */
 
-var planetas = [
+var planeta = [
     {
         nombre:"Star planet",
         x:362,
@@ -14,37 +14,34 @@ var planetas = [
         tamanio: 12,
         color: "#dfd743",
         mostrar: true,
-        velocidad: 0.0107,
+        velocidad: 0.0410,
         pos_rad:0
     }
 ];
-
 var estrellas = [
     {
         nombre:"Alpha Centauri B",
-        h:200,
-        y: 268,
-        r: 80,
-        c: 80,
-        tamanio: 24,
+        x:270,
+        y: 310,
+        orbita: 140,
+        tamanio: 30,
         color: "#6f72df",
         mostrar: true,
-        velocidad: 1,
-        pos_rad:Math.PI/2
+        velocidad: 0.0107,
+        pos_rad:43
     },
     {
         nombre: "Proxima Centauri",
-        h: 200,
-        y: 268,
-        r: 80,
-        c: 80,
-        tamanio: 20,
+        x: 350,
+        y: 380,
+        orbita: 180,
+        tamanio: 25,
         color: "#dadf9e",
         mostrar: true,
-        velocidad: 0.01,
-        pos_rad: Math.PI / 2
+        velocidad: 0.0211,
+        pos_rad:16
     }
-]
+];
 
 function dibujarEstrellas() {
     var canvas = document.getElementById('Canvas');
@@ -53,24 +50,18 @@ function dibujarEstrellas() {
     var cenX = canvas.width / 2;
     var cenY = canvas.height / 2;
 
-    //Elipse
-    ctx.beginPath();
-    ctx.ellipse(cenX - 50, cenY, 160, 170, Math.PI / 2, 0, 2 * Math.PI,false);
-    ctx.stroke();
-    ctx.closePath();
-
-
     //Alpha Centauri A
     ctx.beginPath();
-    ctx.arc(cenX, cenY, 28, 0, 2 * Math.PI, false);
+    ctx.arc(cenX, cenY, 27, 0, 2 * Math.PI, false);
     ctx.fillStyle = "#FFFF00";
     ctx.strokeStyle = "#FFFF00";
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
 
-    for(var i of planetas){
+      for(var i of planeta){
 
+        //orbitaAlphaA
         ctx.beginPath();
         ctx.moveTo(cenX+i.orbita,200);
         ctx.arc(cenX, cenY, i.orbita, 0, 2 * Math.PI, false);
@@ -89,52 +80,84 @@ function dibujarEstrellas() {
             ctx.closePath();
         }
     }
-    for(var i of estrellas){
-        if(i.mostrar) {
 
-            ctx.beginPath();
-            ctx.fillStyle = i.color;
-            ctx.moveTo(i.x, i.y);
-            ctx.ellipse(i.x, i.y,i.tamanio,i.tamanio, Math.PI / 2, 0, 2 * Math.PI,false);
-            ctx.stroke();
-            ctx.fill();
-            ctx.closePath();
-        }
+
+for(var i of estrellas){
+
+    var eje=cenX-50;
+    //orbitaAlphaB
+    ctx.beginPath();
+    ctx.moveTo(eje+i.orbita,200);
+    ctx.arc(eje, cenY, i.orbita, 0, 2 * Math.PI, false);
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+    ctx.closePath();
+
+    if(i.mostrar) {
+        //estrellas
+        ctx.beginPath();
+        ctx.fillStyle = i.color;
+        ctx.moveTo(i.x, i.y);
+        ctx.arc(i.x, i.y, i.tamanio, 0, 2 * Math.PI, false);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
     }
 }
+}
+
+// Validar el año ingresado - alerta
+function validar() {
+    var x = document.forms["formulario"]["year"].value;
+
+    if (x == "" || x<0 || x.length<4 || isNaN(x)==1) {
+        alert("Debe ingresar año valido");
+        for(var i of planeta){
+            i.mostrar=false;
+        }
+    } else{
+        for(var i of planeta){
+            i.mostrar=true;
+        }
+    }
+    return false;
+
+}
+
 var interval;
 
+a=0;
 
 function moverplanetas() {
     clearInterval(interval);
-    var x = document.forms["formulario"]["fechayear"].value/2000;
+    var x = document.forms["formulario"]["year"].value/2000;
 
 
-    for(var i of planetas){
+    for(var i of planeta){
         i.pos_rad += x;
         i.x= 400 + i.orbita* Math.cos(i.pos_rad);
         i.y= 200 + i.orbita* Math.sin(i.pos_rad);
     }
-
-    interval=setInterval(animacion, 10);
-}
-
-function moverElipse() {
-    clearInterval(interval);
-    var x = document.forms["formulario"]["fechayear"].value/2000;
-
     for(var i of estrellas){
-        i.h +=x;
-        i.x = (i.r*i.r)*(Math.cos(i.pos_rad)*Math.cos(i.pos_rad))-(2*i.r*Math.cos(i.pos_rad)*i.c)+(i.c*i.c);
+        i.pos_rad += x;
+        i.x= 350 + i.orbita* Math.cos(i.pos_rad);
+        i.y= 200 + i.orbita* Math.sin(i.pos_rad);
     }
+
     interval=setInterval(animacion, 10);
 }
 
 function animacion(){
-    for(var i of planetas){
+    for(var i of planeta){
         i.pos_rad += i.velocidad;
         i.x= 400 + i.orbita* Math.cos(i.pos_rad);
         i.y= 200 + i.orbita* Math.sin(i.pos_rad);
     }
+    for(var i of estrellas){
+        i.pos_rad += i.velocidad;
+        i.x= 350 + i.orbita* Math.cos(i.pos_rad);
+        i.y= 200 + i.orbita* Math.sin(i.pos_rad);
+    }
+
     dibujarEstrellas();
 }
